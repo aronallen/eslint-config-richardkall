b806ee1d1c65f1e7d6befebc450570ddf6d9ccdc
Richard Käll <richard.kall@me.com>
2015-10-06 23:30:49 +0200

    Add legacy config (ES5)

---
+++ legacy.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'extends': [
+    'eslint-config-richardkall/rules/best-practices',
+    'eslint-config-richardkall/rules/errors',
+    'eslint-config-richardkall/rules/legacy',
+    'eslint-config-richardkall/rules/node',
+    'eslint-config-richardkall/rules/strict',
+    'eslint-config-richardkall/rules/style',
+    'eslint-config-richardkall/rules/variables'
+  ],
+  'env': {
+    'browser': true
+  },
+  'ecmaFeatures': {},
+  'globals': {},
+  'rules': {}
+};
