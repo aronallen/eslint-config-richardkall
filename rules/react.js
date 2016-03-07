d8a6905f59fba4a9f13c62a2442543a111b3d4b5
Richard Käll <richard.kall@me.com>
2016-03-07 14:44:28 +0100

    Add prefer-stateless-function

--- rules/react.js
+++ rules/react.js
@@ -40,6 +40,7 @@ module.exports = {
     'react/no-string-refs': 2,
     'react/no-unknown-property': 2,
     'react/prefer-es6-class': 2,
+    'react/prefer-stateless-function': 2,
     'react/prop-types': 2,
     'react/react-in-jsx-scope': 2,
     'react/require-extension': 0,
