b54363f2be8f08877d02dd62a3a87bc120eaed6e
Richard Käll <richard.kall@me.com>
2016-04-10 20:12:52 +0200

    Add react/require-render-return

--- rules/react.js
+++ rules/react.js
@@ -44,6 +44,7 @@ module.exports = {
     'react/prop-types': 2,
     'react/react-in-jsx-scope': 2,
     'react/require-extension': 0,
+    'react/require-render-return': 2,
     'react/self-closing-comp': 2,
     'react/sort-comp': 2,
     'react/sort-prop-types': [2, {'callbacksLast': true}],
