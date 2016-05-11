a788dae246ab835e3e127e467c94be2c12c3e721
Richard Käll <richard.kall@me.com>
2016-05-11 12:02:01 +0200

    Add jsx-no-target-blank

--- rules/react.js
+++ rules/react.js
@@ -23,6 +23,7 @@ module.exports = {
     'react/jsx-no-bind': [2, {allowArrowFunctions: true}],
     'react/jsx-no-duplicate-props': 2,
     'react/jsx-no-literals': 0,
+    'react/jsx-no-target-blank': 2,
     'react/jsx-no-undef': 2,
     'react/jsx-pascal-case': 2,
     'react/jsx-sort-props': [2, {callbacksLast: true}],
